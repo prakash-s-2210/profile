64f84ed2222176020b6d3ee0fcdc1646698e0265
prakash-s-2210 <sbprakash.kvs@gmail.com>
2023-08-22 12:37:03 +0530

    fix/deployment error

--- next.config.js
+++ next.config.js
@@ -19,19 +19,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 's-maxage=1, stale-while-revalidate=59',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
