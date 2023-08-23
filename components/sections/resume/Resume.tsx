b3d81a53d1e168a0c81ecb99d0274f6402faf794
prakash-s-2210 <sbprakash.kvs@gmail.com>
2023-08-23 17:05:55 +0530

    fix/experience-education-title

--- components/sections/resume/Resume.tsx
+++ components/sections/resume/Resume.tsx
@@ -31,96 +31,104 @@ const Resume = ({ profileData, filteredLanguagesWithImages }: IResumeProps) => {
     <div className="max-md:mx-5 flex flex-col gap-10 mt-10">
       <About profileData={profileData} />
 
-      <section className="flex flex-col gap-5">
-        {profileData.workExperience.length > 0 &&
-          sortedWorkExperience.map((experience) => (
-            <div key={experience.title}>
-              <div className="flex gap-4 max-sm:flex-col p-6 bg-zinc-50 border border-zinc-100 rounded-2xl">
-                <Image
-                  src="/assets/icons/company.png"
-                  alt="default image for work experience"
-                  width={40}
-                  height={40}
-                  className="w-10 h-10"
-                />
-                <div className="flex-1">
-                  <h3 className="text-zinc-900 text-xl font-semibold">
-                    {`${experience.title} at ${experience.companyName}`}
-                  </h3>
+      <section className="flex flex-col gap-6">
+        <h3 className="text-center text-zinc-900 text-2xl font-bold">Work experience</h3>
 
-                  <div className="flex justify-between gap-x-5 gap-y-1 flex-wrap">
-                    <div className="text-zinc-900 text-base flex items-center gap-2">
-                      <p>{experience.location}</p>
+        <div className="flex flex-col gap-5">
+          {profileData.workExperience.length > 0 &&
+            sortedWorkExperience.map((experience) => (
+              <div key={experience.title}>
+                <div className="flex gap-4 max-sm:flex-col p-6 bg-zinc-50 border border-zinc-100 rounded-2xl">
+                  <Image
+                    src="/assets/icons/company.png"
+                    alt="default image for work experience"
+                    width={40}
+                    height={40}
+                    className="w-10 h-10"
+                  />
+                  <div className="flex-1">
+                    <h3 className="text-zinc-900 text-xl font-semibold">
+                      {`${experience.title} at ${experience.companyName}`}
+                    </h3>
 
-                      <span className="bg-zinc-600 h-1 w-1 rounded-full"></span>
+                    <div className="flex justify-between gap-x-5 gap-y-1 flex-wrap">
+                      <div className="text-zinc-900 text-base flex items-center gap-2">
+                        <p>{experience.location}</p>
 
-                      <p>{experience.companyName}</p>
+                        <span className="bg-zinc-600 h-1 w-1 rounded-full"></span>
+
+                        <p>{experience.companyName}</p>
+                      </div>
+
+                      <p className="text-zinc-900 text-base font-semibold">
+                        {experience.present
+                          ? `${formatDateToMonthYear(
+                              experience.startDate
+                            )} - Present`
+                          : `${formatDateToMonthYear(
+                              experience.startDate
+                            )} - ${formatDateToMonthYear(experience.endDate)}`}
+                      </p>
                     </div>
 
-                    <p className="text-zinc-900 text-base font-semibold">
-                      {experience.present
-                        ? `${formatDateToMonthYear(
-                            experience.startDate
-                          )} - Present`
-                        : `${formatDateToMonthYear(
-                            experience.startDate
-                          )} - ${formatDateToMonthYear(experience.endDate)}`}
+                    <p className="text-zinc-500 text-base">
+                      {experience.description}
                     </p>
                   </div>
-
-                  <p className="text-zinc-500 text-base">
-                    {experience.description}
-                  </p>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </section>
 
-      <section className="flex flex-col gap-5">
-        {profileData.education.length > 0 &&
-          sortedEducation.map((education) => (
-            <div key={education.name}>
-              <div className="flex gap-4 max-sm:flex-col p-6 bg-zinc-50 border border-zinc-100 rounded-2xl">
-                <Image
-                  src="/assets/icons/university.png"
-                  alt="default image for work education"
-                  width={40}
-                  height={40}
-                  className="w-10 h-10"
-                />
-                <div className="flex-1">
-                  <h3 className="text-zinc-900 text-xl font-semibold">
-                    {education.name}
-                  </h3>
+      <section className="flex flex-col gap-6">
+        <h3 className="text-zinc-900 text-2xl font-bold">Education</h3>
 
-                  <div className="flex justify-between gap-x-5 gap-y-1 flex-wrap">
-                    <div className="text-zinc-900 text-base flex items-center gap-2">
-                      <p>{education.location}</p>
+        <div className="flex flex-col gap-5">
+          {profileData.education.length > 0 &&
+            sortedEducation.map((education) => (
+              <div key={education.name}>
+                <div className="flex gap-4 max-sm:flex-col p-6 bg-zinc-50 border border-zinc-100 rounded-2xl">
+                  <Image
+                    src="/assets/icons/university.png"
+                    alt="default image for work education"
+                    width={40}
+                    height={40}
+                    className="w-10 h-10"
+                  />
+                  <div className="flex-1">
+                    <h3 className="text-zinc-900 text-xl font-semibold">
+                      {education.name}
+                    </h3>
 
-                      <span className="bg-zinc-600 h-1 w-1 rounded-full"></span>
+                    <div className="flex justify-between gap-x-5 gap-y-1 flex-wrap">
+                      <div className="text-zinc-900 text-base flex items-center gap-2">
+                        <p>{education.location}</p>
 
-                      <p>{education.qualification}</p>
+                        <span className="bg-zinc-600 h-1 w-1 rounded-full"></span>
+
+                        <p>{education.qualification}</p>
+                      </div>
+
+                      <p className="text-zinc-900 text-base font-semibold">
+                        {education.present
+                          ? `${formatDateToMonthYear(
+                              education.startDate
+                            )} - Present`
+                          : `${formatDateToMonthYear(
+                              education.startDate
+                            )} - ${formatDateToMonthYear(education.endDate)}`}
+                      </p>
                     </div>
 
-                    <p className="text-zinc-900 text-base font-semibold">
-                      {education.present
-                        ? `${formatDateToMonthYear(
-                            education.startDate
-                          )} - Present`
-                        : `${formatDateToMonthYear(
-                            education.startDate
-                          )} - ${formatDateToMonthYear(education.endDate)}`}
+                    <p className="text-zinc-500 text-base">
+                      {education.description}
                     </p>
                   </div>
-
-                  <p className="text-zinc-500 text-base">
-                    {education.description}
-                  </p>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </section>
 
       <section className="flex flex-col gap-6">
